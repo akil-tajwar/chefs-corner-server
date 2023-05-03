3dfbe9a8c99e81ff85cd355a0fae7252066dac98
akil-tajwar <akiltajwar@gmail.com>
2023-05-03 19:47:11 +0600

    server site connection done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Dragon is running');
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+app.listen(port, () => {
+    console.log(`Dragon is running on ${port}`);
+})
